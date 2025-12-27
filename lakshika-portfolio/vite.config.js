dd1e8a70f11610b9e56a9eb0fe540b23e5248943
Lakshika Ranaweera <sadumgc123@gmail.com>
2025-12-27 20:48:32 +0530

    vite

--- lakshika-portfolio/vite.config.js
+++ lakshika-portfolio/vite.config.js
@@ -1,19 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Necessary for __dirname to work in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Maps the "@" prefix to the "src" folder
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+  base: "/My-Portfolio/" // <-- repo name
+});
